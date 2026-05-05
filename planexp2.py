b03a106a2a8bc0fde3d51eca520747cefeb78778
timoschwartzdev <timothee.schildknecht@liebherr.com>
2026-05-05 15:06:51 +0200

    Update planexp2.py

--- planexp2.py
+++ planexp2.py
@@ -71,8 +71,13 @@ def read_file(file_name: str, file_bytes: bytes, header_rows: list) -> pd.DataFr
     if file_name.endswith('.csv'):
         df_raw = pd.read_csv(io.BytesIO(file_bytes), header=None)
     else:
-        df_raw = pd.read_excel(io.BytesIO(file_bytes), header=None)
-        engine="openpyxl"
+        df_raw = pd.read_excel(
+            io.BytesIO(file_bytes),
+            header=None,
+            engine="openpyxl",
+            na_values=["#N/A", "#DIV/0!", "#VALUE!", "#REF!", "#NUM!"]
+)
+
     
     # --- ATTENTION : SUPPRIME LA LIGNE QUI ÉTAIT ICI (df_raw = pd.read_excel...) ---
     
